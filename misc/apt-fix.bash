1f6517f72f732bafd6195b16e6b979ca47c195fc
ItsMmmike <113321364+ItsMmmike@users.noreply.github.com>
2025-02-05 10:39:03 -0500

    update fix for additional packages

--- misc/apt-fix.bash
+++ misc/apt-fix.bash
@@ -5,6 +5,7 @@
 
 # Retrieves updated gpg keys from kali archive
 sudo wget https://archive.kali.org/archive-key.asc -O /etc/apt/trusted.gpg.d/kali-archive-keyring.asc;
+sudo wget -qO - https://dl.google.com/linux/linux_signing_key.pub | sudo tee /etc/apt/trusted.gpg.d/google.asc >/dev/null
 
 # Deletes install file after completion
 rm -- "$0"
