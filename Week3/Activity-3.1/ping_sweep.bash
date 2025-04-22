cd8a6db92460b4ac115f7a276a8ae519cabf7317
Mike-User <michael.ng@mymail.champlain.edu>
2025-04-22 10:57:25 -0400

    update

--- Week3/Activity-3.1/ping_sweep.bash
+++ Week3/Activity-3.1/ping_sweep.bash
@@ -5,8 +5,8 @@
 # Display Output
 echo "ip,port"
 
-# Loop through range 2-50, filter results, then return if success
-for ip in $(seq 2 50); do
+# Loop through range 1-254, filter results, then return if success
+for ip in $(seq 1 254); do
   check=$(timeout 0.1 bash -c "echo > /dev/tcp/10.0.5.$ip/53" >/dev/null 2>&1 && echo "up" || echo "");
   if [ -n "$check" ]; then
     echo "10.0.5.$ip,53"
